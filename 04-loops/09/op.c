4422a0e182dc6e2ca17e3dc0305159b38c8b6ee2
Anton <at1.030@gmail.com>
2014-04-01 18:35:05 +0400

    [04-loops]: Oleg Pestov solution of 09 problem fixed (leap years)

--- 04-loops/09/op.c
+++ 04-loops/09/op.c
@@ -2,28 +2,32 @@
 
 int days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+int IsLeap(int y) {
+  return (y % 4 == 0 && y % 100 != 0) || y % 400 == 0;
+}
+
+int DaysInMonth(int m, int y) {
+  return m != 1 ? days[m] : days[m] + IsLeap(y);
+}
+
 int main() {
   int d, m, y, d2, m2, y2;
+  int k = 0; 
+
   scanf("%d%d%d", &d, &m, &y);
   scanf("%d%d%d", &d2, &m2, &y2);
 
-  if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
-    days[1] = 29;
-
-  int k = 0; 
   while (d != d2 || m != m2 || y != y2) {
     k++;
 
     d++;
-    if (d > days[m-1]) {
+    if (d > DaysInMonth(m - 1, y)) {
       d = 1;
       m++;
     } 
     if (m > 12) {
       m = 1;
       y++;
-      if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
-        days[1] = 29;
     }
   }
 
